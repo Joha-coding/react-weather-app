80104ae539e1bd6baa6f66e08f5ce4d56398a81c
Joha <jotz@outlook.fr>
2024-01-15 12:04:41 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/Joha-coding"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Joha
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Joha-coding/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Joha-coding"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Joha
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Joha-coding/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
