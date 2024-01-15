959f990a55fd7c5a20e9566cd8878a06d82a0fa0
Joha <jotz@outlook.fr>
2024-01-15 12:18:56 +0100

    Worked on the html of the new Weather component and created weather.css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
