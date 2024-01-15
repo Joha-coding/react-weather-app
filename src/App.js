95f23f7db38a9792ecd04fb68c519aab613103ae
Joha <jotz@outlook.fr>
2024-01-15 11:04:01 +0100

    Added the Github repo URL in a footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Joha-coding" target="_blank">
+          Joha
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Joha-coding/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
