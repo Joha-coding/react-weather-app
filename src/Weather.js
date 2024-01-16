3de03dbbf9b20092cf59eeed584cce6395dc07af
Joha <jotz@outlook.fr>
2024-01-16 11:30:08 +0100

    Line 5:8:  "WeatherIcon" is defined but never used  no-unused-vars

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
-import WeatherIcon from "./WeatherIcon";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
